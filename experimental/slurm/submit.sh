41d5843af5094eba07760bba9f60df1a4d2d7a1b
DWarez <dario.salvati@domyn.com>
2026-01-21 11:21:01 +0100

    change: slurm launch scripts, sft main to work on slurm
    Signed-off-by: DWarez <dario.salvati@domyn.com>

--- experimental/slurm/submit.sh
+++ experimental/slurm/submit.sh
@@ -6,10 +6,55 @@
 # LICENSE file in the root directory of this source tree.
 
 CONFIG_NAME="${1}"
+RES_DIR="/leonardo_scratch/fast/iGen_train/$USER/forge/logs/$SLURM_JOB_NAME"
 
-sbatch --job-name="${CONFIG_NAME}_controller" \
+export NUM_NODES=1
+export GPUS_PER_NODE=4
+export CPUS_PER_NODE=4
+
+export HF_CACHE_DIR="/leonardo_scratch/fast/iGen_train/$USER/hf_cache"
+export HF_HOME="/leonardo_scratch/fast/iGen_train/$USER/hf_cache"
+export WANDB_MODE=offline
+export TRANSFORMERS_OFFLINE=1
+export HF_DATASETS_OFFLINE=1
+export HF_HUB_OFFLINE=1
+
+# export NCCL_DEBUG=INFO 
+export NCCL_TIMEOUT=600
+export NCCL_DEBUG_SUBSYS=ALL
+export TORCH_NCCL_BLOCKING_WAIT=1
+export CUDA_LAUNCH_BLOCKING=1
+
+export MASTER_PORT=9251
+
+
+CONDA_PATH="/leonardo/home/userexternal/$USER/miniconda3"
+FORGE_DIR="/leonardo_scratch/fast/iGen_train/$USER/forge"
+
+export TORCH_COMPILE_DISABLE=1
+unset SLURM_MEM_PER_CPU SLURM_MEM_PER_GPU SLURM_MEM_PER_NODE
+export TORCHSTORE_RDMA_ENABLED=0
+
+export MONARCH_LOG_LEVEL=DEBUG
+export TMPDIR="/leonardo_scratch/fast/iGen_train/$USER/tmp"
+export XDG_RUNTIME_DIR="/leonardo_scratch/fast/iGen_train/$USER/tmp"
+export TEMPDIR="/leonardo_scratch/fast/iGen_train/$USER/tmp"
+mkdir -p "$TMPDIR"
+
+
+sbatch --verbose --job-name="${CONFIG_NAME}_controller" \
        --export=ALL,CONFIG_NAME="${CONFIG_NAME}" \
-       experimental/slurm/submit_grpo.sh
+       --account=AIFAC_L07_016 \
+       --qos=qos_llm_min \
+       --partition=boost_usr_prod \
+       --time=00:30:00 \
+       --nodes=${NUM_NODES} \
+       --gres=gpu:${GPUS_PER_NODE} \
+       --cpus-per-task=${CPUS_PER_NODE} \
+       --exclude="lrdn[1831-3456]" \
+       --error="$RES_DIR/%x_%j.err" \
+       --output="$RES_DIR/%x_%j.out" \
+       experimental/slurm/custom_submit_sft.sh
 
 
 # Usage:
